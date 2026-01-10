0fc3ee0815bb8d8e031f1ba162f345d689ce3614
cooperbirdsall <cooper_birdsall@brown.edu>
2026-01-09 16:26:52 -0800

    add players

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import { Button, Flex, TextInput } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [players, setPlayers] = useState([]);
+  const [playerInputValue, setPlayerInputValue] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const playerList = players.map(player => {
+    return <p style={{margin: 0}} key={player[0]}>{player[0]}</p>
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider defaultColorScheme='dark'>
+      {!gameStarted && 
+        <Flex gap="xl">
+        <Flex direction="column">
+          <h3 style={{margin: 0}}>Players: {players.length}</h3>
+          {playerList}
+        </Flex>
+        <Flex direction="column" gap="md">
+          <Flex gap="sm">
+            <TextInput autoFocus value={playerInputValue} onChange={(event) => setPlayerInputValue(event.currentTarget.value)} placeholder="Name"></TextInput>
+            <Button color="orange" onClick={() => {
+              const currentPlayers = players;
+              if (playerInputValue.length > 0) {
+                currentPlayers.push([playerInputValue, 50]);
+                setPlayers(currentPlayers);
+                setPlayerInputValue("");
+              }
+            }}>Add</Button>
+          </Flex>
+          <Button color="orange" w={150} onClick={() => {setGameStarted(true)}}>Start Game</Button>
+        </Flex>
+      </Flex>
+      }   
+    </MantineProvider>
   )
 }
 
